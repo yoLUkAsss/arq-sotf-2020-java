ce1e8003b494b6366ab3859e9e84d7c0988b8347
atk-exilio <sandoval.lucasj@gmail.com>
2020-06-24 01:23:35 -0300

    Add - #2 crear entidad insumo

---
+++ src/main/java/com/coronavirus/insumos/repository/InputRepository.java
@@ -0,0 +1,7 @@
+package com.coronavirus.insumos.repository;
+
+import com.coronavirus.insumos.model.Input;
+
+public interface InputRepository extends BaseRepository<Input> {
+
+}
--- src/main/test/com/coronavirus/insumos/UserServiceTest.java
+++ src/main/test/com/coronavirus/insumos/UserServiceTest.java
@@ -8,8 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.coronavirus.insumos.service.UserService;
 import com.coronavirus.insumos.users.User;
-import com.coronavirus.insumos.users.UserService;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
